0267298ebe2b040895b984d7828b390841db2904
manisanthosh <manisanthosh.b@gmail.com>
2026-03-31 12:03:07 +0530

    Update coding_task.py

--- src/tasks/coding_task.py
+++ src/tasks/coding_task.py
@@ -3,7 +3,7 @@ from crewai import Task
 def create_coding_task(agent, feature_request):
     return Task(
         description=f"""
-        Build a any function for the following requirement:
+        Build a any programe/function for the following requirement:
 
         {feature_request}
 
